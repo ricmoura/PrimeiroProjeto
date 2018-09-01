9cef3de654c0314bb3a4db13de75b55da15abdfb
AulaDroid01-09 <ricardo_moura@outlook.com>
2018-09-01 12:21:20 -0300

    configuracao da tela de resultado

--- app/src/main/java/com/example/ricardo/primeiroprojeto/ResultadoActivity.kt
+++ app/src/main/java/com/example/ricardo/primeiroprojeto/ResultadoActivity.kt
@@ -2,8 +2,11 @@ package com.example.ricardo.primeiroprojeto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
+import com.example.ricardo.primeiroprojeto.extensions.format
 import com.example.ricardo.primeiroprojeto.utils.ConstantesExtra
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
@@ -15,7 +18,53 @@ class ResultadoActivity : AppCompatActivity() {
         val peso = intent.extras.getString(ConstantesExtra.KEY_PESO)
         val altura = intent.extras.getString(ConstantesExtra.KEY_ALTURA)
 
+        calcularIMC(peso.toDouble(),altura.toDouble())
 
-        Toast.makeText(this, peso, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun calcularIMC(peso: Double, altura: Double) {
+        val imc = peso / (altura*altura)
+
+        if (imc < 17) {
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.magreza)
+        }else if(imc < 18.5){
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.abaixo)
+        }else if(imc <24.9){
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.ideal)
+        }else if(imc < 29.9){
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.sobre)
+        }else if(imc < 34.9){
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.obesidade)
+        }else if(imc < 39.9){
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.obesidade)
+        }else {
+            tvIMC.text = imc.format(0)
+            setImgaem(R.drawable.obesidade)
+        }
+
+
+        /*when (imc){
+            in 0.0..17.0 ->{tvIMC.setText(imc.toString())
+                            setImgaem(R.drawable.magreza)}
+            in 17.1..18.5 ->{            tvIMC.setText(imc.toString())
+                setImgaem(R.drawable.abaixo)}
+            in 18.6..24.9 ->{            tvIMC.setText(imc.toString())
+                setImgaem(R.drawable.ideal)}
+            in 25.0..34.9 ->{}
+            in 35.0..39.9 ->{}
+            else -> {}
+        }*/
+    }
+
+    private fun setImgaem(resourceId: Int){
+        ivIMC.setImageDrawable(
+                ContextCompat.getDrawable(this, resourceId)
+        )
     }
 }
---
+++ app/src/main/java/com/example/ricardo/primeiroprojeto/extensions/DoubleExt.kt
@@ -0,0 +1,3 @@
+package com.example.ricardo.primeiroprojeto.extensions
+
+fun Double.format(digitos: Int) = String.format("%.${digitos}f%",this)
